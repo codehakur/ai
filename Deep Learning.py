1abd9e66dab5e69334dd24a66029a076843bd4b1
codehakur <105128702+codehakur@users.noreply.github.com>
2022-05-11 08:21:10 +0530

    Update Deep Learning.py

--- Deep Learning.py
+++ Deep Learning.py
@@ -23,4 +23,4 @@ print('Accuracy: %.2f' % (accuracy*100))
 predictions = (model.predict(X) > 0.5).astype(int)
 # summarize the first 5 cases
 for i in range(5):
-print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+  print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
